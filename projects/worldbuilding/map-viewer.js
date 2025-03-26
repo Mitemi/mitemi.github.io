b62cc3636db1e124998266a988d53007f8e60724
Mitemi <miter360@gmail.com>
2025-03-26 12:06:22 -0400

    Update

--- projects/worldbuilding/map-viewer.js
+++ projects/worldbuilding/map-viewer.js
@@ -1,3 +1,8 @@
+const readMaplist = (url) => fetch(url)
+    .then(response => response.json());
+
+const mapList = await readMaplist('../../archive/uchronie/maplist.json')
+
 const forward = document.getElementById("forward");
 const backward = document.getElementById("backward");
 const checkText = document.getElementById("text");
@@ -6,21 +11,13 @@ const auto = document.getElementById("auto");
 const centerView = document.querySelector(".center-view");
 const map = centerView.querySelector('img');
 
-let mapList = [
-    "../../archive/uchronie/maps/70_shardedv4-relief.PNG",
-    "../../archive/uchronie/maps/74_shardedv4-113.PNG",
-    "../../archive/uchronie/maps/75_shardedv4-190.PNG",
-    "../../archive/uchronie/maps/76_shardedv4-270.PNG",
-    "../../archive/uchronie/maps/77_shardedv4-291.PNG"
-];
-
 let mapIndex = 0;
 
 const mapUpdate = v => {
     if (v < 0) mapIndex = 0;
     if (v > mapList.length - 1) mapIndex = mapList.length - 1;
 
-    map.src = checkText.checked ? mapList[mapIndex].replace(".PNG", "-text.PNG") : mapList[mapIndex];
+    map.src = checkText.checked ? mapList[mapIndex].map.replace(".PNG", "-text.PNG") : mapList[mapIndex].map;
 };
 
 forward.addEventListener('click', () => {
@@ -34,7 +31,8 @@ backward.addEventListener('click', () => {
 });
 
 auto.addEventListener('click', async () => {
-    for (let i = 0; i <= mapList.length - mapIndex + 1; i++) {
+    const startIndex = mapIndex;
+    for (let i = 0; i <= mapList.length - startIndex + 1; i++) {
         mapIndex++;
         mapUpdate(mapIndex);
         await new Promise(res => setTimeout(res, 2000));

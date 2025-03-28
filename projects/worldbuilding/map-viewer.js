95d36f9d69f713f224ee650ba7d25eea10b1f908
Mitemi <miter360@gmail.com>
2025-03-28 12:54:24 -0400

    Update Map Viewer

--- projects/worldbuilding/map-viewer.js
+++ projects/worldbuilding/map-viewer.js
@@ -10,15 +10,27 @@ const auto = document.getElementById("auto");
 
 const centerView = document.querySelector(".center-view");
 const map = centerView.querySelector('img');
+const year = document.querySelector(".infobox p:nth-child(1)");
+const info = document.querySelector(".infobox p:nth-child(2)");
+const mapSelect = document.querySelector("select");
 
 let mapIndex = 0;
 
 const mapUpdate = v => {
-    if (v < 0) mapIndex = 0;
+    if (v < 0 || mapSelect.value != "none") mapIndex = 0;
     if (v > mapList.length - 1) mapIndex = mapList.length - 1;
 
-    map.src = checkText.checked ? mapList[mapIndex].map.replace(".PNG", "-text.PNG") : mapList[mapIndex].map;
+    if (mapSelect.value == "none") {
+        map.src = checkText.checked ? mapList[mapIndex].map.replace(".PNG", "-text.PNG") : mapList[mapIndex].map;
+    } else {
+        map.src = checkText.checked ? mapSelect.value.replace(".PNG", "-text.PNG") : mapSelect.value;
+    }
+
+    year.innerText = mapSelect.value != "none" ? "" : mapList[mapIndex].year;
+    info.innerText = mapSelect.value != "none" ? "" : mapList[mapIndex].info;
 };
+
+mapSelect.onchange = () => mapUpdate(mapIndex);
 
 forward.addEventListener('click', () => {
     mapIndex++;
